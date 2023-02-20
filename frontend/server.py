d40bbc0d13c1e2213decec61fde3cf126c9a032b
NerosJourney <sandman4264@gmail.com>
2023-02-19 23:16:24 -0500

    add categories page on frontend

--- frontend/server.py
+++ frontend/server.py
@@ -5,9 +5,16 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/categories")
+def categories():
+    r = requests.get(f'http://localhost:2502/categories')
+    categories_obj = json.loads(r.text)
+    return render_template('categories.html', categories=categories_obj['categories'])
+
+@app.route("/map")
 def home():
-    r = requests.get(f'http://localhost:2502/conferences?category=AI')
+    category = request.args.get('category')
+    r = requests.get(f'http://localhost:2502/conferences?category={category}')
     conferences_obj = json.loads(r.text)
     return render_template('map.html', conferences=conferences_obj['conferences'])
 
